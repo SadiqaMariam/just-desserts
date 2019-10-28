ae62beb912420242ae9bbf12956ccf00272f405a
SadiqaMariam <sadiqamariam@hotmail.com>
2019-10-29 01:33:10 +0800

    Add order Id to order received page with styling

--- orderreceived.php
+++ orderreceived.php
@@ -32,6 +32,7 @@
         <link rel="stylesheet" href="stylesheets/footer.css">
         <link rel="stylesheet" href="stylesheets/pageContent.css">
         <link rel="stylesheet" href="stylesheets/orderProgress.css">
+        <link rel="stylesheet" href="stylesheets/orderReceived.css">
     </head>
     <body>
         <?php echo getHeader('orderreceived'); ?>
@@ -40,6 +41,25 @@
             <div class="pageActiveContent">
                 <?php 
                     echo getOrderProgress('received');
+                    echo
+<<<HTML
+                    <div class="orderReceivedWrapper">
+                        <div class="orderReceviedContent">
+                            <div class="orderReceivedIdHeader">Order Id</div>
+                            <div class="orderReceivedId">{$orderId}</div>
+                            <div class="orderReceivedRemark">
+                                <p>
+                                    * Note : Please save this order id to track your order. We have also sent an notification email to you. 
+                                </p>
+                            </div>
+                            <div class="orderReceivedBackToMenuButtonWrapper">
+                                <button class="button orderReceivedBackToMenuButton">
+                                    <a href="menu.php">Go to Menu</a>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+HTML;
                 ?>
             </div>
         </div>
