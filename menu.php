80e6e4ae77e54d453b38c43b3034f250b48f271c
Sadiqamariam <sadiqamariam@hotmail.com>
2019-10-29 23:04:27 +0800

    Modify menu page with menu options and images

--- menu.php
+++ menu.php
@@ -12,16 +12,35 @@
         <link rel="stylesheet" href="stylesheets/header.css">
         <link rel="stylesheet" href="stylesheets/footer.css">
         <link rel="stylesheet" href="stylesheets/pageContent.css">
+        <link rel="stylesheet" href="stylesheets/menu.css">
     </head>
     <body>
         <?php echo getHeader('menu'); ?>
 
         <div class="pageContent">
             <div class="pageActiveContent">
-                <p>Menu Page</p>
-                <a href="cakes.php">Cakes<a>
-                <a href="icecreams.php">Ice-creams<a>
-                <a href="cupcakes.php">Cup cakes<a>
+                <div class="menuWrapper">
+                    <div class="menuContent">
+                        <div class="menuOptionWrapper menuPageCakesWrapper">
+                            <a href="cakes.php">
+                                <img src="images/menuCakes.png"><br><br>
+                                <span>Cakes</span>
+                            </a>
+                        </div>
+                        <div class="menuOptionWrapper menuPageIcecreamsWrapper">
+                            <a href="icecreams.php">
+                                <img src="images/menuIceCreams.png"><br><br>
+                                <span>Ice Creams</span>
+                            </a>
+                        </div>
+                        <div class="menuOptionWrapper menuPageCupcakesWrapper">
+                            <a href="cupcakes.php">
+                                <img src="images/menuCupcakes.png"><br><br>
+                                <span>Cup cakes</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </body>
