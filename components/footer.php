b3920a19089aa1f3bc8d134f0cf08021af67b733
Sadiqamariam <sadiqamariam@hotmail.com>
2019-11-02 17:51:56 +0800

    Add links to footer

--- components/footer.php
+++ components/footer.php
@@ -6,25 +6,25 @@
                 <div class="footerNavigate">
                     <p class="footerTitles">Navigations</p>
                     <div class="footerOptions footerNavigationOptions">
-                        <a href="">Home</a>
-                        <a href="">Menu</a>
-                        <a href="">Contact</a>
+                        <a href="home.php">Home</a>
+                        <a href="menu.php">Menu</a>
+                        <a href="contact.php">Contact</a>
                     </div>
                 </div>
                 <div class="footerMenu">
                     <p class="footerTitles">Menu</p>
                     <div class="footerOptions footerMenuOptions">
-                        <a href="">Cupcakes</a>
-                        <a href="">Ice Creams</a>
-                        <a href="">Cakes</a>
+                        <a href="cupcakes.php">Cupcakes</a>
+                        <a href="icecreams.php">Ice Creams</a>
+                        <a href="cakes.php">Cakes</a>
                     </div>
                 </div>
                 <div class="footerContactUs">
                     <p class="footerTitles">Contact</p>
                     <div class="footerOptions footerContactOptions">
-                        <a href="">Tel : +6161-6161</a>
-                        <a href="">Email : justdesserts@mail.com</a>
-                        <a href="">Provide feedback</a>
+                        <a href="contact.php">Tel : +6556-6556</a>
+                        <a href="contact.php">Email : justdesserts@mail.com</a>
+                        <a href="contact.php">Provide feedback</a>
                     </div>
                 </div>
                 <div class="footerLogo">
